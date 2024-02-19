58c38f680260725484077afe3e2e4b15ec4f5bad
Ayush kumar <ayushkumar@adsl-ull-231-0.51-151.wind.it>
2024-02-20 01:02:57 +0530

    files added

---
+++ netflix.js
@@ -0,0 +1,12 @@
+let username = document.getElementById('text2')
+let password= document.getElementById('password2')
+
+function submit(){
+  if(username==""){
+    alert ("error")
+  }else if(password==""){
+    alert("error")
+  }else{
+    alert (username + "user name enter")
+  }
+}
